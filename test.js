1ba1726f62c839081ee9b47dd1454016b586982f
lilgreml1n <davidcorbett@gmail.com>
2020-11-19 18:02:33 +0000

    lab2

---
+++ test.js
@@ -0,0 +1,7 @@
+var local_color = "green";
+var local_getIpv6 = function(ipv4) {
+  console.log(`Parameter ipv4 has value ${ipv4}`);
+};
+
+module.exports.color = local_color;
+module.exports.getIpv6 = local_getIpv6;
